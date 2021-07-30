4bbf00d61d06afd7e6456bed916e2cbc295822d3
milocortes <hermilocg@hotmail.com>
2021-07-30 11:58:19 -0500

    actualiza surrogate

--- referencias/propuestas/surrogates/surrogates_ml.py
+++ referencias/propuestas/surrogates/surrogates_ml.py
@@ -1,4 +1,4 @@
-#### This file contains the Python source code of the functions used for the paper "Agent-Based Model Calibration using Machine  Learning Surrogates" by Francesco Lamperti, Andrea Roventini and Amir Sani. 
+#### This file contains the Python source code of the functions used for the paper "Agent-Based Model Calibration using Machine  Learning Surrogates" by Francesco Lamperti, Andrea Roventini and Amir Sani.
 #### copyright of Amir Sani, Francesco Lamperti and Andrea Roventini.
 
 
@@ -23,20 +23,20 @@ from scipy.stats.distributions import entropy
 from xgboost import XGBRegressor, XGBClassifier
 
 # Gaussian Process Regression (Kriging)
-import pyKriging  
-from pyKriging.krige import kriging  
+import pyKriging
+from pyKriging.krige import kriging
 from pyKriging.samplingplan import samplingplan
 
 """ cross-validation
-Cross validation is used in each of the rounds to approximate the selected 
-surrogate model over the data samples that are available. 
-The evaluated parameter combinations are randomly split into two sets. An 
-in-sample set and an out-of-sample set. The surrogate is trained and its 
-parameters are tuned to an in-sample set, while the out-of-sample performance 
-is measured (using a selected performance metric) on the out-of-sample set. 
-This out-of-sample performance is then used as a proxy for the performance 
-on the full space of unevaluated parameter combinations. In the case of the 
-proposed procedure, this full space is approximated by the randomly selected 
+Cross validation is used in each of the rounds to approximate the selected
+surrogate model over the data samples that are available.
+The evaluated parameter combinations are randomly split into two sets. An
+in-sample set and an out-of-sample set. The surrogate is trained and its
+parameters are tuned to an in-sample set, while the out-of-sample performance
+is measured (using a selected performance metric) on the out-of-sample set.
+This out-of-sample performance is then used as a proxy for the performance
+on the full space of unevaluated parameter combinations. In the case of the
+proposed procedure, this full space is approximated by the randomly selected
 pool.
 """
 from sklearn.model_selection import cross_val_score, StratifiedKFold
@@ -64,87 +64,87 @@ def evaluate_islands_on_set(parameter_combinations):
     0,10), # rho
     (0.8,2), # alpha
     (0.0,1.0), # phi
-    (0.0,1.0), # pi                                     
+    (0.0,1.0), # pi
     (0.0,1.0), # eps
     (10,100), # N
     (0.0,1.0)]) # Lambda
     """
     y = np.zeros(parameter_combinations.shape[0])
     num_params = parameter_combinations.shape[1]
-    
+
     if num_params == 1:
         for i,rho in enumerate(parameter_combinations):
-            gdp = island_abm(rho=rho, 
+            gdp = island_abm(rho=rho,
                              _RNG_SEED=0)
-            y[i] = calibration_measure(gdp)    
+            y[i] = calibration_measure(gdp)
     elif num_params == 2:
         for i,(rho,alpha) in enumerate(parameter_combinations):
             gdp = island_abm(rho=rho, alpha=alpha,
                              _RNG_SEED=0)
             y[i] = calibration_measure(gdp)
-        
+
     elif num_params == 3:
         for i,(rho,alpha,phi) in enumerate(parameter_combinations):
-            gdp = island_abm(rho=rho, alpha=alpha, phi=phi, 
+            gdp = island_abm(rho=rho, alpha=alpha, phi=phi,
                              _RNG_SEED=0)
             y[i] = calibration_measure(gdp)
-        
+
     elif num_params == 4:
         for i,(rho,alpha,phi,pi) in enumerate(parameter_combinations):
-            gdp = island_abm(rho=rho, alpha=alpha, phi=phi, 
+            gdp = island_abm(rho=rho, alpha=alpha, phi=phi,
                              pi=pi, _RNG_SEED=0)
             y[i] = calibration_measure(gdp)
-        
+
     elif num_params == 5:
         for i,(rho,alpha,phi,pi,eps) in enumerate(parameter_combinations):
-            gdp = island_abm(rho=rho, alpha=alpha, phi=phi, 
+            gdp = island_abm(rho=rho, alpha=alpha, phi=phi,
                              pi=pi, eps=eps, _RNG_SEED=0)
             y[i] = calibration_measure(gdp)
-        
+
     elif num_params == 6:
         for i,(rho,alpha,phi,pi,eps,N) in enumerate(parameter_combinations):
-            gdp = island_abm(rho=rho, alpha=alpha, phi=phi, 
+            gdp = island_abm(rho=rho, alpha=alpha, phi=phi,
                              pi=pi, eps=eps, N=int(N),
                              _RNG_SEED=0)
-            y[i] = calibration_measure(gdp)        
+            y[i] = calibration_measure(gdp)
     elif num_params == 7:
         for i,(rho,alpha,phi,pi,eps,N,lambda_param) in enumerate(parameter_combinations):
-            gdp = island_abm(rho=rho, alpha=alpha, phi=phi, 
-                             pi=pi, eps=eps, N=int(N), 
+            gdp = island_abm(rho=rho, alpha=alpha, phi=phi,
+                             pi=pi, eps=eps, N=int(N),
                              lambda_param=lambda_param,
                              _RNG_SEED=0)
             y[i] = calibration_measure(gdp)
-        
+
     return y
 
-def island_abm(rho=0.01, 
-               alpha=1.5, 
-               phi=0.4, 
-               pi=0.4, 
-               eps=0.1, 
-               lambda_param=1, 
-               T=100, 
-               N=50, 
+def island_abm(rho=0.01,
+               alpha=1.5,
+               phi=0.4,
+               pi=0.4,
+               eps=0.1,
+               lambda_param=1,
+               T=100,
+               N=50,
                _RNG_SEED=0):
     """ Islands growth model
     Parameters
     ----------
-    rho : 
-    
-    alpha : 
-    
+    rho :
+
+    alpha :
+
     phi : float, required
-    
-    eps : 
-    
+
+    eps :
+
     lambda_param: (Default = 1)
-        
+
     T : int, required
     The number of periods for the simulation
-    
+
     N : int, optional (Default = 50)
     Number of firms
-    
+
     _RNG_SEED : int, optional (Default = 0)
     Random number seen
     Output
@@ -153,15 +153,15 @@ def island_abm(rho=0.01,
     Simulated GPD
     """
     # Set random number seed
-    np.random.seed(_RNG_SEED) 
+    np.random.seed(_RNG_SEED)
 
     T_2 = int(T/2)
-    
+
     GDP = np.zeros((T, 1))
 
     # Distributions
     # Precompute random binomial draws
-    xy = np.random.binomial(1, pi, (T, T)) 
+    xy = np.random.binomial(1, pi, (T, T))
     xy[T_2, T_2] = 1
 
     # Containers
@@ -174,7 +174,7 @@ def island_abm(rho=0.01,
     m = np.zeros((T, T))
     m[T_2, T_2] = N
     dest = np.zeros((N, 2))
-    
+
     """ Begin ABM Code """
     for t in range(T):
         w = np.zeros((N, N))
@@ -254,25 +254,25 @@ def island_abm(rho=0.01,
                         dest[i,1] = A[ind,2]
 
         GDP[t, 0] = np.sum(A[:, 4])
-    
+
     log_GDP = np.log(GDP)
-    
+
     return log_GDP
 
 def calibration_measure(log_GDP):
     """ Calibration Measure
-    
+
     Input
     -----
-    
+
     GDP : array, required, length = [,T]
-    
+
     Output
     ------
-    
+
     agdp : float
     Average GDP growth rate
-    
+
     """
     T = log_GDP.shape[0]
     log_GDP = log_GDP[~np.isinf(log_GDP)]
@@ -289,29 +289,29 @@ def calibration_condition(average_GDP_growth_rate,threshold_condition):
 
 def set_surrogate_as_gbt():
     """ Set the surrogate model as Gradient Boosted Decision Trees
-    Helper function to set the surrogate model and parameter space 
+    Helper function to set the surrogate model and parameter space
     as Gradient Boosted Decision Trees.
-    
-    For detail, see: 
+
+    For detail, see:
     http://scikit-learn.org/stable/modules/generated/
     sklearn.ensemble.GradientBoostingRegressor.html
-    
-    
+
+
     Parameters
     ----------
-    
+
     None
-    
+
     Returns
     -------
-    
-    surrogate_model : 
-    
-    surrogate_parameter_space : 
-    
-    
+
+    surrogate_model :
+
+    surrogate_parameter_space :
+
+
     """
-    
+
     surrogate_model = XGBRegressor(seed=0)
 
     surrogate_parameter_space = [
@@ -321,59 +321,59 @@ def set_surrogate_as_gbt():
         (0.0, 100), # reg_alpha
         (0.0, 100), # reg_lambda
         (1, 1000)] # scale_pos_weight
-        
+
     return surrogate_model, surrogate_parameter_space
 
 def set_surrogate_as_gpr():
     """ Set the surrogate model as Gaussian Process Regression (Kriging)
     Helper function to set the surrogate model and parameter space for
     Gaussian Process Regression (Kriging)
-    
-    For detail, see: 
+
+    For detail, see:
     http://scikit-learn.org/stable/modules/generated/
     sklearn.gaussian_process.GaussianProcessRegressor.html
-    
-    Parameters are optimized using L-BFGS from the data and a Radial Basis 
+
+    Parameters are optimized using L-BFGS from the data and a Radial Basis
     Function is used as the Kernel.
-    
+
     Parameters
     ----------
-    
+
     None# Set the surrogaet parameter settings to the output of surrogate_model_tuned
-    
+
     Returns
     -------
-    
-    surrogate_model : 
-    
-    surrogate_parameter_space : 
-    
-    
+
+    surrogate_model :
+
+    surrogate_parameter_space :
+
+
     """
-    
+
     surrogate_model = GaussianProcessRegressor(random_state=0)
 
     surrogate_parameter_space  = []
-    
+
     return surrogate_model, surrogate_parameter_space
 
 def fit_surrogate_model(X,y, surrogate_model, surrogate_parameter_space):
     """ Fit a surrogate model to the X,y parameter combinations
-    
+
     Parameters
     ----------
-    
-    surrogate_model : 
-    
-    X : 
-    
-    y : 
-    
-    
+
+    surrogate_model :
+
+    X :
+
+    y :
+
+
     Output
     ------
-    surrogate_model_fitted : A surrogate model fitted 
-    
+    surrogate_model_fitted : A surrogate model fitted
+
     """
     def objective(params):
         n_estimators, learning_rate, max_depth, reg_alpha, reg_lambda,  \
@@ -386,19 +386,19 @@ def fit_surrogate_model(X,y, surrogate_model, surrogate_parameter_space):
                             reg_lambda=reg_lambda,
                             scale_pos_weight=scale_pos_weight,
                             seed=0)
-        
-        return -np.mean(cross_val_score(reg, 
-                                        X, y, 
-                                        cv=_N_SPLITS, 
+
+        return -np.mean(cross_val_score(reg,
+                                        X, y,
+                                        cv=_N_SPLITS,
                                         n_jobs=-1,
                                         scoring="neg_mean_squared_error"))
 
-    from skopt import gp_minimize    
+    from skopt import gp_minimize
 
-    # We assume the surrogate model parameter space is smooth, so we 
+    # We assume the surrogate model parameter space is smooth, so we
     # use Gaussian Process Regression to optimize the Hyper-Parameters.
-    surrogate_model_tuned = gp_minimize(objective, 
-                                        surrogate_parameter_space, 
+    surrogate_model_tuned = gp_minimize(objective,
+                                        surrogate_parameter_space,
                                         n_calls=_N_EVALS,
                                         n_points=100000,
                                         n_jobs=1,
@@ -413,29 +413,29 @@ def fit_surrogate_model(X,y, surrogate_model, surrogate_parameter_space):
                                seed=0)
 
     surrogate_model.fit(X,y)
-    
+
     return surrogate_model
 
 def fit_entropy_classifier(X,y, surrogate_model, surrogate_parameter_space):
     """ Fit a surrogate model to the X,y parameter combinations
-    
+
     Parameters
     ----------
-    
-    surrogate_model : 
-    
-    X : 
-    
-    y : 
-    
-    
+
+    surrogate_model :
+
+    X :
+
+    y :
+
+
     Output
     ------
-    surrogate_model_fitted : A surrogate model fitted 
-    
+    surrogate_model_fitted : A surrogate model fitted
+
     """
     y_binary = calibration_condition(evaluated_set_y,calibration_threshold)
-    
+
     def objective(params):
         n_estimators, learning_rate, max_depth, reg_alpha, reg_lambda,  \
             scale_pos_weight = params
@@ -447,23 +447,23 @@ def fit_entropy_classifier(X,y, surrogate_model, surrogate_parameter_space):
                             reg_lambda=reg_lambda,
                             scale_pos_weight=scale_pos_weight,
                             seed=0)
-                
-        skf = StratifiedKFold(n_splits=_N_SPLITS,random_state=0,shuffle=False)
-        skf_cv = [(train,test) for train,test in skf.split(X,y_binary)]        
 
-        return -np.mean(cross_val_score(clf, 
-                                        X, y_binary, 
-                                        cv=skf_cv, 
+        skf = StratifiedKFold(n_splits=_N_SPLITS,random_state=0,shuffle=False)
+        skf_cv = [(train,test) for train,test in skf.split(X,y_binary)]
+
+        return -np.mean(cross_val_score(clf,
+                                        X, y_binary,
+                                        cv=skf_cv,
                                         n_jobs=-1,
                                         scoring="f1_weighted"))
 
-    from skopt import gp_minimize    
+    from skopt import gp_minimize
 
-    # We assume the surrogate model parameter space is smooth, so we 
+    # We assume the surrogate model parameter space is smooth, so we
     # use Gaussian Process Regression to optimize the Hyper-Parameters.
-    clf_tuned = gp_minimize(objective, 
-                            surrogate_parameter_space, 
-                            n_calls=_N_EVALS, 
+    clf_tuned = gp_minimize(objective,
+                            surrogate_parameter_space,
+                            n_calls=_N_EVALS,
                             n_points=100000,
                             n_jobs=1,
                             random_state=0)
@@ -475,13 +475,13 @@ def fit_entropy_classifier(X,y, surrogate_model, surrogate_parameter_space):
                         reg_lambda=clf_tuned.x[4],
                         scale_pos_weight=clf_tuned.x[5],
                         seed=0)
-    
+
     clf.fit(X,y_binary)
-    
+
     return clf
 
 def run_online_surrogate(budget, n_dimensions, islands_exploration_range, calibration_threshold):
-    print "XGBoost Started!"
+    print("XGBoost Started!")
     # 1. Draw the Pool
     # Set pool size
     pool_size = 1000000
@@ -506,7 +506,7 @@ def run_online_surrogate(budget, n_dimensions, islands_exploration_range, calibr
     unevaluated_set_X = pool[list(set(range(pool_size)) - set(initialization_samples))]
 
     surrogate_model, surrogate_parameter_space = set_surrogate_as_gbt()
-    print "Evaluated set size: ", evaluated_set_y.shape[0]
+    print("Evaluated set size: ", evaluated_set_y.shape[0])
 
     while evaluated_set_y.shape[0] < budget:
         # 3. Build Surrogate on evaluated samples
@@ -538,20 +538,20 @@ def run_online_surrogate(budget, n_dimensions, islands_exploration_range, calibr
 
 def get_sobol_samples(n_dimensions, samples, parameter_support):
     """
-    
-    
+
+
     """
     # Get the range for the support
     support_range = parameter_support[:,1] - parameter_support[:,0]
-    
+
     # Generate the Sobol samples
     random_samples = sobol_seq.i4_sobol_generate(n_dimensions, samples)
-    
+
     # Compute the parameter mappings between the Sobol samples and supports
     sobol_samples = np.vstack([
-        np.multiply(s,support_range)+parameter_support[:,0] 
+        np.multiply(s,support_range)+parameter_support[:,0]
                for s in random_samples])
-    
+
     return sobol_samples
 
 def get_round_selections(evaluated_set_X,evaluated_set_y,
@@ -560,12 +560,12 @@ def get_round_selections(evaluated_set_X,evaluated_set_y,
                          samples_to_select,
                          budget):
     """
-    
-    
+
+
     """
     samples_to_select = np.min([abs(budget - evaluated_set_y.shape[0]),
                                 samples_to_select]).astype(int)
-    
+
     if num_predicted_positives>=samples_to_select:
         round_selections = int(samples_to_select)
         selections = np.where(predicted_positives==True)[0]
@@ -577,7 +577,7 @@ def get_round_selections(evaluated_set_X,evaluated_set_y,
 
         # select remainder according to entropy weighting
         budget_shortfall = int(samples_to_select - num_predicted_positives)
-        
+
         selections = np.append(selections,
                                get_new_labels_entropy(evaluated_set_X, evaluated_set_y,
                                                       unevaluated_set_X,
@@ -590,7 +590,7 @@ def get_round_selections(evaluated_set_X,evaluated_set_y,
     unevaluated_set_X = np.delete(unevaluated_set_X,selections,0)
     evaluated_set_X = np.vstack([evaluated_set_X, to_be_evaluated])
     evaluated_set_y = np.append(evaluated_set_y,evaluate_islands_on_set(to_be_evaluated))
-    
+
     return evaluated_set_X, evaluated_set_y, unevaluated_set_X
 
 def get_new_labels_entropy(evaluated_set_X, evaluated_set_y,
@@ -598,24 +598,24 @@ def get_new_labels_entropy(evaluated_set_X, evaluated_set_y,
                            number_of_new_labels,
                            _KRIGING = 0):
     """ Get a set of parameter combinations according to their predicted label entropy
-    
-    
-    
+
+
+
     """
     if _KRIGING:
         clf = GaussianProcessClassifier()
         clf.fit(evaluated_set_X,calibration_condition(evaluated_set_y,calibration_threshold))
     else:
-        clf = fit_entropy_classifier(evaluated_set_X,evaluated_set_y, 
+        clf = fit_entropy_classifier(evaluated_set_X,evaluated_set_y,
                                      surrogate_model, surrogate_parameter_space)
 
     y_hat_probability = clf.predict_proba(unevaluated_X)
     y_hat_entropy = np.array(map(entropy,y_hat_probability))
     y_hat_entropy /= y_hat_entropy.sum()
     unevaluated_X_size = unevaluated_X.shape[0]
-        
+
     selections = np.random.choice(a = unevaluated_X_size,
                                   size = number_of_new_labels,
                                   replace = False,
-                                  p = y_hat_entropy)    
+                                  p = y_hat_entropy)
     return selections
